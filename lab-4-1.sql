d79ce25944013bda06686f1ebfc4d05ea6cf986c
Vivek Nair <vivek.nair@kellogg.northwestern.edu>
2025-01-16 02:58:45 +0000

    Solved 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,11 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT sum(hits) 
+FROM stats stat
+INNER JOIN players player on player.id = stat.player_id 
+WHERE player.first_name = "Barry" 
+AND player.last_name="Bonds"; 
+
 -- Expected result:
 -- 2935
 
